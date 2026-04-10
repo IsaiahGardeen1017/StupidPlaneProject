89f8ba45cc201b03371dd996f88f0febd889ec9a
Isaiah Gardeen <igardeen@meditech.com>
2026-04-10 10:01:11 -0500

    Finished first pass at plane stats

--- engine/typescriptBuild/planes.ts
+++ engine/typescriptBuild/planes.ts
@@ -1,7 +1,13 @@
 import { PlaneStats } from "./utils";
 
 export const planes: Record<string, PlaneStats> = {
-    'bf109' = {
-
+    'bf109': {
+        weight: 2700,
+        liftCoefficientByAoa: [{x: -5, y: 0.0}, {x: 0, y: 0.45}, {x: 5, y: 0.9}, {x: 10, y: 1.35}, {x: 15, y: 1.45}, {x: 25, y: 0.8}], //Don't know if reasonable
+        wingSurfaceArea: 16.2,
+        parasiteDragReferenceArea: 0.3,
+        parasiteDragCoefficient: 0.03,
+        thrustByAltitude: [{x: 1.2250, y: 8000}, {x: 0.905, y: 7000}],
+        AspectRatioOswaldConstant: 6.14 * 0.75
     }
 }
--- engine/typescriptBuild/utils.ts
+++ engine/typescriptBuild/utils.ts
@@ -3,19 +3,10 @@ export type CurvePoint2D = {
     y: number;
 };
 
-export type CurvePoint3D = {
-    x: number;
-    y: number;
-};
-
 export type Curve2D = CurvePoint2D[];
-export type Curve3D = CurvePoint3D[];
 
 export type PlaneStats = {
     weight: number; //kg
-    thrustBySpeedAndAltitude: Curve2D; //Speed as input
-    dragBySpeedAndAltitude: Curve3D; //Speed, Altitude
-
     /*
         Lift Equation: L = (1/2 * p) * V^2 * S * C
         L: Total Lift
@@ -26,24 +17,33 @@ export type PlaneStats = {
 
         We simplify this to: L = 0.5 * V^2 * cs
         We will assume angle of attack is always flat for simplicty for now
-        Each plane type will have a single lift stat, which will represent Surface area * Lift Coefficent
-
+        Each plane type will have a single lift stat, cs, which will represent Surface area * Lift Coefficent
     */
-    liftCoefficient: number;
+    liftCoefficientByAoa: Curve2D; // constant, depending on Angle of Attack
+    wingSurfaceArea: number; //meter squared
 
-    thrust;
+    /*
+        Parasite Drag equation: D = 1/2 * p * V^2 * Cdo * S
+        p: Air Density
+        V: Velocity
+        Cdo: Zero Lift Drag Coefficient
+        S: Reference Area
+    */
+    parasiteDragReferenceArea: number; //meters squared
+    parasiteDragCoefficient: number; //constant
+
+    /*
+        For now we will keep thrust simple, it will only depend on altitude.  In the future we might add a speed dimension
+        Output Value is in newtons
+    */
+    thrustByAltitude: Curve2D;
+
+    /*
+        Induced drag equation
+        Di = 1/2 * p * V^2 * S * Dci
+        Dci = (C ^ 2) / (pi * AR * e)
+
+        We already use C (liftCoefficientByAoa) and we will need a new stat (AR * e) 
+    */
+   AspectRatioOswaldConstant: number;
 };
-
-export function generateFlat2DCurve(
-    minx: number,
-    maxx: number,
-    flatValue: number,
-): Curve2D {
-    return [{
-        x: minx,
-        y: flatValue,
-    }, {
-        x: maxx,
-        y: flatValue,
-    }];
-}
